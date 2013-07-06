4b3cf9cce4c174d94a421dc79d8e6a99c8de49ae
Jonathan Hoyt <hoyt@github.com>
2013-07-05 20:35:34 -0700

    Refactor runner.

--- run.rb
+++ run.rb
@@ -5,8 +5,10 @@ require_relative "machine"
 require_relative "multiply"
 require_relative "number"
 
-expression = Add.new(Multiply.new(Number.new(1), Number.new(2)), Multiply.new(Number.new(3), Number.new(4)))
-Machine.new(expression).run
+expressions = []
+expressions << Add.new(Multiply.new(Number.new(1), Number.new(2)), Multiply.new(Number.new(3), Number.new(4)))
+expressions << LessThan.new(Number.new(5), Add.new(Number.new(2), Number.new(2)))
 
-expression = LessThan.new(Number.new(5), Add.new(Number.new(2), Number.new(2)))
-Machine.new(expression).run
+expressions.each do |expression|
+  Machine.new(expression).run
+end
