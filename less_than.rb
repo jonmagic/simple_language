3c5e63b368c0039c8d64d006d3186a96cde6ecb6
Jonathan Hoyt <hoyt@github.com>
2013-07-05 20:29:17 -0700

    Add Boolean and LessThan.

---
+++ less_than.rb
@@ -0,0 +1,23 @@
+class LessThan < Struct.new(:left, :right)
+  def to_s
+    "#{left} < #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      self.class.new(left.reduce, right)
+    elsif right.reducible?
+      self.class.new(left, right.reduce)
+    else
+      Boolean.new(left.value < right.value)
+    end
+  end
+end
--- run.rb
+++ run.rb
@@ -1,7 +1,12 @@
-require_relative "machine"
-require_relative "number"
 require_relative "add"
+require_relative "boolean"
+require_relative "less_than"
+require_relative "machine"
 require_relative "multiply"
+require_relative "number"
 
 expression = Add.new(Multiply.new(Number.new(1), Number.new(2)), Multiply.new(Number.new(3), Number.new(4)))
+Machine.new(expression).run
+
+expression = LessThan.new(Number.new(5), Add.new(Number.new(2), Number.new(2)))
 Machine.new(expression).run
